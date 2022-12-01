87906c88cd5cb11188cf545970505739e2023b09
Joseph Collard <jcollard@users.noreply.github.com>
2022-12-01 07:30:05 -0700

    chore: Initial project prototype.

---
+++ Adventure Prototype/Assets/Adventure/ILocation.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ILocation 
+{
+    public string Name { get; }
+    public string Description { get; }
+}
---
+++ Adventure Prototype/Assets/Adventure/LocationUIController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LocationUIController : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI Name;
+    public TMPro.TextMeshProUGUI Description;
+
+}
